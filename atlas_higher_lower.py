cb3e5e1976e211098a7f0ed329c8fb9f4606c330
ajiteshchanna <141831972+ajiteshchanna@users.noreply.github.com>
2024-04-18 07:30:23 +0530

    Update atlas_higher_lower.py
    1. Some new features are added such as tracking the user's name so that we can store it with his high score, this makes the game more competing and interesting.
    
    2. No country will be displayed for the second time and country A and country B will never be the same.

--- atlas_higher_lower.py
+++ atlas_higher_lower.py
@@ -1,59 +1,138 @@
 from art import logo_atlas, vs
 from atlas import data
-import random
 import os
-
+import random
+import time
+import pandas
 
 clear = lambda: os.system('cls')
+appeard_countries = []
 
 
+# create a print header function to print header for each of the turn
 def print_header(A, B, score):
     print(logo_atlas)
     if score > 0:
-        print(f"You are Right!: Your Score is {score}")
+        print(f"You are right✅: Current Score = {score}")
     print(f"Compare A: {A['name']}")
     print(vs)
     print(f"Against B: {B['name']}")
 
 
+# create a function play again
+def play_again():
+    again = input("Type 'y' to play again and 'n' to exit the game: ")
+    if again.lower() == 'y':
+        return True
+    elif again.lower() == 'n':
+        return False
+    else:
+        print("INVALID INPUT, TRY AGAIN.")
+        play_again()
+
+
+# game function which allows the user to play the game
 def game():
-    curr_score = 0
+    # create a current_score variable
+    current_score = 0
+
+    # take name as input and store it in a variable called name
+    user_name = input("Enter your name: ")
+    clear()
+
+    # random choice of countries A and B
     A = random.choice(data)
     B = random.choice(data)
+
+    # if A == B the computer should choose again
+    while A == B:  # so now no two same countries will be shown
+        B = random.choice(data)
     game_is_on = True
+
+    # then it should print the welcome message
+    print(f"Hi {user_name}, welcome to the: ")
     while game_is_on:
-        print_header(A, B, curr_score)
-        user = input("Which country has more population? Type 'A' or 'B': ")
-        ans = "A" if A['population'] >= B['population'] else "B"
-        if user == ans:
-            curr_score += 1
-            if user == "A":  # means our A had more followers
+        # printing the header
+        print_header(A, B, score=current_score)
+
+        # taking choice from the user 
+        user_choice = input("Which country has higher population? 'A' or 'B': ")
+        answer = "A" if A['population'] > B['population'] else "B"
+
+        # if the user is correct
+        if user_choice.upper() == answer:
+            appeard_countries.append(A['name'])
+            appeard_countries.append(B['name'])
+            current_score += 1
+
+            if user_choice == "A":  # means our A had more followers
                 A = B
                 B = random.choice(data)
+                while B['name'] in appeard_countries:
+                    B = random.choice(data)
+
             else:  # means our B had more followers
                 B = random.choice(data)
-                clear()
-        else:
-            game_is_on = False
-            print("Sorry, You are Wrong, GAME OVER💀")
-            # updating the high score
-            with open("highscore.txt", mode="r") as file:
-                high_score = file.read()
-                if high_score == '':
-                    high_score=0
-                if int(high_score) < curr_score:
-                    high_score = curr_score
-            with open("highscore.txt", mode="w") as file:
-                file.write(f"{high_score}")
-            print(f"YOUR SCORE: {curr_score} \nHIGH SCORE: {high_score}")
+                while B['name'] in appeard_countries:
+                    B = random.choice(data)
+            clear()
 
-    again = input("Do you want to play again? Type 'y' for yes, 'n' for no: ")
-    if again.lower() == 'y':
-        clear()
-        game()
-    else:
-        clear()
-        return
+        # the user is wrong, but not invalid
+        elif user_choice != answer and (user_choice == "A" or user_choice == "B"):
+            game_is_on = False
+            print("Sorry, You are wrong. GAME OVER💀")
+
+            # reading the high score
+            high_score = pandas.read_csv('highscore.csv')
+            names = []
+            score = []
+            for index, row in high_score.iterrows():
+                names.append(row['name'])
+                score.append(row['score'])
+
+            # checking whether the user is now or has played earlier
+            if user_name in names:
+                prev_score = score[names.index(user_name)]
+                if current_score > prev_score:
+                    prev_score = current_score
+                    score[names.index(user_name)] = prev_score
+                your_high_score = prev_score
+
+                # saving the user's score
+                high_score.loc[high_score['name'] == user_name, 'score'] = your_high_score
+                high_score.to_csv('highscore.csv', index=False)
+            else:
+                names.append(user_name)
+                score.append(current_score)
+                your_high_score = current_score
+
+                new_score_data = {'name': user_name,
+                                  'score': your_high_score}
+
+                # creating a data frame
+                new_player = pandas.DataFrame({'name': [user_name], 'score': [your_high_score]}, index=[0])
+                high_score = high_score._append(new_player, ignore_index=True)
+                high_score.to_csv('highscore.csv', index=False)
+
+            # now finding the highscore
+            highscore_score = max(score)
+            high_score_player = names[score.index(highscore_score)]
+
+            print(f"Your Score: {current_score}")
+            print(f"Your HighScore: {your_high_score}")
+            print(f"Overall HighScore-> {high_score_player} : {highscore_score}")
+
+            # asking if the user wants to play the game again or not
+            again = play_again()
+            if again:
+                clear()
+                game()
+            else:
+                print("Thank you for playing the Higher Lower Atlas Game🌏.")
+                return
+        else:
+            print("Invalid Input! Try again")
+            time.sleep(1.5)  # letting the INVALID INPUT MESSAGE TO DISPLAY FOR SOME TIME
 
 
 game()
